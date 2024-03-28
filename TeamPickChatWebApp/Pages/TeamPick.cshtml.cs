65d8fbfba942a30f21da59309043956f9e81b947
mishutrz2 <mishutrz2@yahoo.com>
2024-03-29 00:29:50 +0200

    initial project structure

---
+++ TeamPickChatWebApp/Pages/TeamPick.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TeamPickChatWebApp.Pages
+{
+    public class TeamPickModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
